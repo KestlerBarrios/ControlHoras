e9bbd6e84a519f5c28520058ff8f474c666d45fb
Kestler Barrios <luiseduardo3213@gmail.com>
2020-10-22 23:47:41 -0600

    Inicio de Proyecto

---
+++ ControlDeHoras/Models/Control_Semanal.cs
@@ -0,0 +1,7 @@
+namespace ControlDeHoras.Models
+{
+    public class Control_Semanal
+    {
+        
+    }
+}
---
+++ ControlDeHoras/Models/Tipo_Actividad.cs
@@ -0,0 +1,7 @@
+namespace ControlDeHoras.Models
+{
+    public class Tipo_Actividad
+    {
+        
+    }
+}
